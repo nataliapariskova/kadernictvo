1034057e2967da54d5cd6084d12fa32d53ad87d5
nataliapariskova <47498089+nataliapariskova@users.noreply.github.com>
2019-04-29 11:42:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,6 @@
 include 'pages/header.php';
 include 'pages/connect.php';
 
-//$sql = "SELECT * FROM auto";
-//$result = $conn->query($sql);
-
-
-
 if($_GET["link"]) include 'pages/'.$_GET["link"];
 else include 'pages/home.php';
 
